95452cd80e9053789e02752ab15295665e85201f
Srinithi Doddapaneni <you@example.com>
2025-10-16 15:17:34 -0700

    feat: add comments

--- src/blog.ts
+++ src/blog.ts
@@ -31,30 +31,44 @@ many problems become easier to solve.`,
         slug: "the-magic-of-recursion"
     }
 ]
+//container element where blog posts are displayed
 const blogContainer = document.getElementById('blog-container');
+
+// Loop through the array of blog objects and create HTML elements for each one
 blogs.forEach((blog) => {
+
+    // Create a container <div> element for each blog post
     const blogDiv = document.createElement('div');
-    blogDiv.className="blog-post-container"
+    blogDiv.className = "blog-post-container"; // Assign a class to the div for styling
 
+    // Create the title element 
     const title = document.createElement("h1");
-    title.textContent = blog.title;
+    title.textContent = blog.title; // Set the title of the blog
 
+    // Create the date element 
     const date = document.createElement("h2");
-    date.textContent = blog.date;
+    date.textContent = blog.date; // Set the publication date of the blog
 
+    // Create the description element  
     const description = document.createElement("p");
-    description.textContent = blog.description;
+    description.textContent = blog.description; // Set the description of the blog
 
+    // Create the image element 
     const image = document.createElement("img");
-    image.src = blog.image;
-    image.alt = blog.imageAlt;
+    image.src = blog.image; 
+    image.alt = blog.imageAlt; 
 
+    //anchor tag that will link to the individual blog page
     const blogLink = document.createElement('a');
-    blogLink.href = "blogs/" + blog.slug + ".html";;
-    
-    
+    blogLink.href = "blogs/" + blog.slug + ".html"; // Set the URL using the slug (unique identifier for the blog)
+
+    //append the title, date, image, and description to the blog container div
     blogDiv.append(title, date, image, description);
+
+    //making the div clickable
     blogLink.append(blogDiv);
+
+    // append the anchor link to the main blog container in the HTML
     if (blogContainer)
         blogContainer.append(blogLink);
 });
