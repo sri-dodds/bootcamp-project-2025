e4e3207f637c4fc5e8e3fb05c66200be151c910b
Srinithi Doddapaneni <you@example.com>
2025-10-29 18:46:00 -0700

    bugs fixed

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -2,22 +2,8 @@ import type { Metadata } from "next"; // Object to set metadata
 import { Inter } from "next/font/google"; // You can change the font to anything you want.
 import localFont from "next/font/local";
 import "./globals.css";
-
-
-// If you are experiencing an error "localFont is undefined", you might need to add the following blocks of code
+import Navbar from "../components/navbar";
 const inter = Inter({ subsets: ["latin"] });
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-//
 
 export const metadata: Metadata = {
   title: "[name] Personal Website",
